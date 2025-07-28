917bc615f1aa1f6373a15d14f06ebd785ba590a1
saiganeshnaidu <saiganeshnaiduthota@gmail.com>
2025-07-28 22:47:48 +0530

    Test Case-4: sum of multiple comma-separated values

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,5 +14,13 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1,2")).to eq(3)
     end
 
+    it 'returns sum for multiple comma-separated numbers' do
+      expect(StringCalculator.add("1,2,3,4")).to eq(10)
+    end
+
+    it 'supports new line as a delimiter' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
   end
 end
